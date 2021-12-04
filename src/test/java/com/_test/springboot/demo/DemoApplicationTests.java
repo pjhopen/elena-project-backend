e0b41d1bda20b5ade9a3ae4b34a789c97afcf9a6
Philip Hopen <phopen@umass.edu>
2021-12-04 16:19:35 -0500

    Initialize backend project

---
+++ src/test/java/com/_test/springboot/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com._test.springboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
