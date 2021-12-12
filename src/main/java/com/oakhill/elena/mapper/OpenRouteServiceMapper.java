7809771cc00f64c9da96c35c2b918db9da7c3b2d
jurgen <tigeryhw@gmail.com>
2021-12-11 23:57:41 -0500

    implement mapper

--- src/main/java/com/oakhill/elena/mapper/OpenRouteServiceMapper.java
+++ src/main/java/com/oakhill/elena/mapper/OpenRouteServiceMapper.java
@@ -1,9 +1,53 @@
 package com.oakhill.elena.mapper;
 
+
+import java.io.StringReader;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Response;
+
+import com.oakhill.elena.configuration.OpenStreetServiceConfig;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 /**
  * OpenRouteServiceMapper
  */
+
+@Component
 public class OpenRouteServiceMapper {
-    
-    
+
+    @Autowired
+    OpenStreetServiceConfig openStreetServiceConfig;
+
+    public JsonObject queryShortestPathWithWeight() {
+
+        Client client = ClientBuilder.newClient();
+
+        Entity<String> payload = Entity.json(
+                "{\"coordinates\":[[8.681495,49.41461],[8.686507,49.41943]],\"alternative_routes\":{\"target_count\":3,\"weight_factor\":1.4}}");
+
+        //
+        Response response = client.target("https://api.openrouteservice.org/v2/directions/driving-car/geojson")
+                .request()
+                .header("Authorization", openStreetServiceConfig.getApi_key())
+                .header("Accept", "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8")
+                .header("Content-Type", "application/json; charset=utf-8")
+                .post(payload);
+                
+        JsonReader jsonReader = Json.createReader(new StringReader(response.readEntity(String.class)));
+        JsonObject responseBody = jsonReader.readObject();
+
+        // Map<String, List<String>> map = response.readEntity(new
+        // GenericType<Map<String, List<String>>>() { });
+        // List<String> values = map.get("features");
+
+        return responseBody;
+    }
 }
