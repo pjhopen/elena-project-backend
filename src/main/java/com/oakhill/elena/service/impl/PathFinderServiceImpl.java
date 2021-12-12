d545def9f1e97cd0373e2861a7d0fbe3fb2dd662
jurgen <tigeryhw@gmail.com>
2021-12-11 23:57:50 -0500

    implement service

--- src/main/java/com/oakhill/elena/service/impl/PathFinderServiceImpl.java
+++ src/main/java/com/oakhill/elena/service/impl/PathFinderServiceImpl.java
@@ -1,5 +1,35 @@
 package com.oakhill.elena.service.impl;
 
-public class PathFinderServiceImpl {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+
+import com.oakhill.elena.mapper.OpenRouteServiceMapper;
+import com.oakhill.elena.model.Path;
+import com.oakhill.elena.service.intf.PathFinderService;
+import com.oakhill.elena.util.Converters;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PathFinderServiceImpl implements PathFinderService {
+
+    @Autowired
+    OpenRouteServiceMapper openRouteServiceMapper;
+
+    public Path get2dShortestPathSerive() {
+        JsonObject resBody = openRouteServiceMapper.queryShortestPathWithWeight();
+        JsonArray features = resBody.getJsonArray("features");
+        List<double[][]> routes = new ArrayList<>();
+        for (int i = 0; i < features.size(); i++) {
+            JsonObject f = features.getJsonObject(i);
+            JsonObject geometry = f.getJsonObject("geometry");
+            routes.add(Converters.geometryToPath(geometry));
+        }
+        return new Path(routes);
+        
+    }
 }
--- src/main/java/com/oakhill/elena/service/intf/PathFinderService.java
+++ src/main/java/com/oakhill/elena/service/intf/PathFinderService.java
@@ -1,5 +1,7 @@
 package com.oakhill.elena.service.intf;
 
+import com.oakhill.elena.model.Path;
+
 public interface PathFinderService {
-    
+    public Path get2dShortestPathSerive();
 }
