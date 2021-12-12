ab82845caa5d79717461e357c7f6795ac4013e32
jurgen <tigeryhw@gmail.com>
2021-12-11 23:55:38 -0500

    define request type

---
+++ src/main/java/com/oakhill/elena/model/RequestData.java
@@ -0,0 +1,31 @@
+package com.oakhill.elena.model;
+
+public class RequestData {
+    double[] start;
+    double[] end;
+    double weight;
+
+    public double[] getStart() {
+        return this.start;
+    }
+
+    public void setStart(double[] start) {
+        this.start = start;
+    }
+
+    public double[] getEnd() {
+        return this.end;
+    }
+
+    public void setEnd(double[] end) {
+        this.end = end;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
