fcda2cd4e9708e5c092caafa81ca942420fca29b
jurgen <tigeryhw@gmail.com>
2021-12-13 22:00:21 -0500

    update README

---
+++ README.md
@@ -0,0 +1,8 @@
+## Requirements:
+    JavaSE-11
+    Maven-3.8.4
+## Launch: 
+    mvn spring-boot:run
+## Build:
+    mvn clean
+    mvn install
--- src/main/java/com/oakhill/elena/controller/PathFinderController.java
+++ src/main/java/com/oakhill/elena/controller/PathFinderController.java
@@ -11,10 +11,8 @@ import com.oakhill.elena.service.intf.PathFinderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
